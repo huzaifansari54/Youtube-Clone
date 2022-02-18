218cb8260d4410053902c9d8f18592b819d4d397
huzaifansari54 <huzaifans666@gmail.com>
2022-02-18 22:08:40 +0530

    updating...

--- lib/Services/Service.dart
+++ lib/Services/Service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'dart:io';
 
-import 'package:flutter_youtube_ui/Models/Video_By_Id.dart';
+import 'package:flutter_youtube_ui/Models/VideoModel.dart';
 import 'package:flutter_youtube_ui/Services/resource.dart';
 
 import 'package:http/http.dart' as http;
@@ -12,36 +12,7 @@ class ApiService {
 
   static final ApiService instance = ApiService._instantiate();
   static String _baseUrl = 'youtube.googleapis.com';
-  String token = '';
   String base = '/youtube/v3/videos';
-
-  Future<VideoById> fetchVideo({required String videoId}) async {
-    VideoById _video = VideoById(
-        kind: 'null',
-        etag: 'null',
-        items: [],
-        pageInfo: PageInfo(resultsPerPage: 1, totalResults: 0));
-    Map<String, String> parms = {
-      "part": "snippet,contentDetails,statistics",
-      "id": videoId,
-      "key": Resources.API_KEY
-    };
-    Uri url = Uri.https(_baseUrl, base, parms);
-    Map<String, String> headers = {
-      HttpHeaders.contentTypeHeader: 'application/json'
-    };
-    var response = await http.get(url, headers: headers);
-
-    if (response.statusCode == 200) {
-      print(response);
-      _video = VideoById.fromJson(json.decode(response.body));
-      print(_video.items.first.snippet.title);
-      return _video;
-    } else {
-      print("error");
-      return _video;
-    }
-  }
 
   Future<List<Item>> getMostPopularList() async {
     List<Item> videos = [];
@@ -92,7 +63,7 @@ class ApiService {
 
       return URL;
     }
-    throw ('erroe');
+    throw (response.statusCode);
   }
 
   Future<List<Item>> getSubcribers() async {
